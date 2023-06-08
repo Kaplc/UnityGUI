9547a427de70cd44f59a4c15500a32efa190f3e5
Kaplc <755536175@qq.com>
2023-06-08 13:36:25 +0800

    完成图片

---
+++ Assets/Script/Control/Texture.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Texture : BaseControl
+{
+    public ScaleMode scaleMode;
+
+    protected override void StyleOnShow()
+    {
+        GUI.DrawTexture(GUIPos.Position, Content.image, scaleMode);
+    }
+
+    protected override void StyleOffShow()
+    {
+        GUI.DrawTexture(GUIPos.Position, Content.image, scaleMode);
+    }
+}
